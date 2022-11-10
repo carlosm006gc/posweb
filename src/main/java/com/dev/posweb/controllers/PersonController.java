48960377d61b2ef4cd559fa1259e198cb88bf4b5
carlosm006gc <104165201+carlosm006gc@users.noreply.github.com>
2022-11-10 10:49:19 -0300

    change person and delete person

--- src/main/java/com/dev/posweb/controllers/PersonController.java
+++ src/main/java/com/dev/posweb/controllers/PersonController.java
@@ -1,10 +1,13 @@
 package com.dev.posweb.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dev.posweb.domain.Person;
@@ -32,4 +35,22 @@ public class PersonController {
 		personRepository.save(person);
 		return "redirect:/person";
 	}
+	@GetMapping("/person/change/{id}")
+	public String changePerson(@PathVariable("id") Long id, Model model) {
+		Optional<Person> personOp = personRepository.findById(id);
+		if(personOp.isEmpty()) {
+			throw new IllegalArgumentException("Invalid Person");
+		}
+		model.addAttribute("person", personOp.get());
+		return "person/form";
+		}
+	@GetMapping("/person/delete/{id}")
+	public String deletePerson(@PathVariable("id") Long id) {
+		Optional<Person> personOp = personRepository.findById(id);
+		if(personOp.isEmpty()) {
+			throw new IllegalArgumentException("Invalid Person");
+		}
+		personRepository.delete(personOp.get());
+		return "redirect:/person";
+		}
 }
